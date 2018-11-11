351eb21763a6655cd45967fc3f3ec78e3f48e69d
iqbalmaulanaardi <iqbalmaulana.ardi@gmail.com>
2018-11-11 19:20:22 +0700

    ready deply

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -14,6 +14,7 @@ class Middleware {
         }
     }
     static isLogin(req, res, next) {
+        // console.log('dari middleware', req.headers)
         if (req.headers["token"] !== undefined) {
             let decoded = Helper.verifyToken(req.headers["token"]);
             Customer.findOne({
