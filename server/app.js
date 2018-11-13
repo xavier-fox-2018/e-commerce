68b07ccd6f2d131b39860b96a8e0ffd0c650e454
Indira Dayo <indiradayo@live.com>
2018-11-13 12:04:05 +0700

    update 4

--- server/app.js
+++ server/app.js
@@ -3,13 +3,14 @@ const express = require('express'),
       mongoose = require('mongoose'),
       cors = require('cors'),
       db = mongoose.connection,
-      dbURI = `mongodb://<dbuser>:<dbpassword>@ds151463.mlab.com:51463/e-commerce`
+      dbURI = `mongodb://${process.env.dbuser}:${process.env.dbpassword}@ds151463.mlab.com:51463/e-commerce`
+      dbLOCAL =`mongodb://localhost:27017/ecommerce`
 
       app = express(),
       port = process.env.PORT || 3000
 
 mongoose
-    .connect('mongodb://localhost:27017/ecommerce', {useNewUrlParser:true})
+    .connect(dbLOCAL, {useNewUrlParser:true})
 db
     .on('error', console.error.bind(console, 'database connection error:'))
     .once('open', function() {
